8c5207560292ad19e6d227146e59c18955ff0140
Ui <kyh571021@gmail.com>
2024-02-09 11:02:27 +0900

    TIL-20240209

--- BOJ/25826.cpp
+++ BOJ/25826.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <string.h>
 
 using namespace std;
 
@@ -10,14 +9,12 @@ int main() {
     int n, m;
     cin >> n >> m;
 
-    int a[1001][1001];
-    int p[1001][1001];
-    
+    int a[1010][1010];
+    int p[1010][1010];
 
-    int b[m];
 
-    for(int i=0; i<n; i++)
-        for(int j=0; j<n; j++)
+    for(int i=1; i<=n; i++)
+        for(int j=1; j<=n; j++)
             cin >> a[i][j];
 
     int c, i1, j1, i2, j2, k;
@@ -26,29 +23,32 @@ int main() {
         cin >> c;
         if(c == 1) {
             cin >> i1 >> j1 >> i2 >> j2 >> k;
-            memset(p, 0, sizeof(p));
+            i1++; i2++; j1++; j2++;
             p[i1][j1] += k;
             p[i2+1][j2+1] += k;
             p[i1][j2+1] -= k;
             p[i2+1][j1] -= k;
-            for(int j = j1+1; j <= j2+1; j++) {
-                p[i1][j] = p[i1][j-1] + p[i1][j];
-                p[i2+1][j] = p[i2+1][j-1] + p[i2+1][j];
+        }
+        else if(c == 2) {
+            for(int i = 1; i <= n; i++) {
+                for(int j = 2; j <= n; j++) {
+                    p[i][j] = p[i][j-1] + p[i][j];
+                }
             }
-            for(int j = j1; j <= j2+1; j++) {
-                for(int i = i1+1; i <= i2+1; i++) {
+            for(int j = 1; j <= n; j++) {
+                for(int i = 2; i <= n; i++) {
                     p[i][j] = p[i][j] + p[i-1][j];
                 }
             }
-            for(int i=i1; i <= i2; i++) {
-                for(int j=j1; j <= j2; j++) {
+            for(int i=1; i <= n; i++) {
+                for(int j=1; j <= n; j++) {
                     a[i][j] += p[i][j];
                 }
             }
-        }
-        else if(c == 2) {
+
             cin >> i1 >> j1 >> i2 >> j2;
             int sum=0;
+            i1++; i2++; j1++; j2++;
             for(int i=i1; i <= i2; i++) {
                 for(int j=j1; j <= j2; j++) {
                     sum += a[i][j];
